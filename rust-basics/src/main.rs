40c4bf976789ae67526072b5724a9359114de6d5
chisty <mchisty31@gmail.com>
2024-07-29 12:17:34 +0600

    added variables

--- rust-basics/src/main.rs
+++ rust-basics/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    let string="my name is";
+    let num=2;
+    print!("{}",string);
+    print!("your name:{}",num);
+
+    let weight=1;
+    let cost=13.45;
+
 }
+
