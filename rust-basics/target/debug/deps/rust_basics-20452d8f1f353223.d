3a1eb10e02c26e493d524a99d1fab3db2f57a8d3
chisty <mchisty31@gmail.com>
2024-07-29 11:58:19 +0600

    first

---
+++ rust-basics/target/debug/deps/rust_basics-20452d8f1f353223.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Desktop\Rust Basics\rust-basics\target\debug\deps\librust_basics-20452d8f1f353223.rmeta: src/main.rs
+
+C:\Users\USER\Desktop\Rust Basics\rust-basics\target\debug\deps\rust_basics-20452d8f1f353223.d: src/main.rs
+
+src/main.rs:
---
+++ rust-basics/target/debug/deps/rust_basics-291e6a3006ae9a0c.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Desktop\Rust Basics\rust-basics\target\debug\deps\librust_basics-291e6a3006ae9a0c.rmeta: src/main.rs
+
+C:\Users\USER\Desktop\Rust Basics\rust-basics\target\debug\deps\rust_basics-291e6a3006ae9a0c.d: src/main.rs
+
+src/main.rs:
